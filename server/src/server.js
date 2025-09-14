5550893dcd65f148f242aa832d55b062ae9fa2f6
Pratyush Negi <91714625+1234Pratyush@users.noreply.github.com>
2025-09-14 16:58:33 +0530

    cors setup

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoute);
 
